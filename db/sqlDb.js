d8d68d0ff801400d58689945b3c3883d5c1d798d
Eric Pei <ericspei@gmail.com>
2022-11-10 16:48:44 -0800

    ec2

--- db/sqlDb.js
+++ db/sqlDb.js
@@ -1,11 +1,20 @@
-const postgres = require('postgres')
+require('dotenv').config();
+const postgres = require('postgres');
+
+// const sql = postgres({
+//   host: 'localhost',
+//   port: '5432',
+//   database: 'products',
+//   username: '',
+//   password: ''
+// });
 
 const sql = postgres({
-  host: 'localhost',
-  port: '5432',
-  database: 'products',
-  username: '',
-  password: ''
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  pasword: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
 
 module.exports.sql = sql;
